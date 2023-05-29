7e1db203eb71853889885a297960580c8c40fbbe
Yen Lin <lynn575938@outlook.com>
2023-05-29 19:57:19 +0800

    fix: correct typing error in models/restaurant.js

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const restaurantSchema = new Schema({
-  name: { type: String, require: true },
-  name_en: { type: String, require: true },
-  category: { type: String, require: true },
-  image: { type: String, require: true },
-  location: { type: String, require: true },
-  phone: { type: String, require: true },
+  name: { type: String, required: true },
+  name_en: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  location: { type: String, required: true },
+  phone: { type: String, required: true },
   google_map: { type: String, required: true },
   rating: { type: Number, required: true },
   description: { type: String, required: true },

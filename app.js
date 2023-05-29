d362205f2b97a70bcc3a0d00a149965a09cd1568
Yen Lin <lynn575938@outlook.com>
2023-05-29 20:07:10 +0800

    feat: add create and update restaurant function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const Restaurant = require('./models/restaurant')
+const restaurant = require('./models/restaurant')
 
 // 僅在非正式環境時，使用dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -26,12 +27,15 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// // setting template engine
+// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// // setting static files
+// setting static files
 app.use(express.static('public'))
+
+// 直接從 express 呼叫 body-parser
+app.use(express.urlencoded({ extended: true }))
 
 // routes setting
 app.get('/', (req, res) => {
@@ -41,6 +45,19 @@ app.get('/', (req, res) => {
     .catch(err => console.error(err))
 })
 
+// 新增餐廳
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const newRestaurant = req.body
+  Restaurant.create(newRestaurant)
+    .then(() => res.redirect('/'))
+    .catch(err => console.error(err))
+})
+
+// 瀏覽特定餐廳
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant_id = req.params.restaurant_id
   Restaurant.findById(restaurant_id)
@@ -49,6 +66,7 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
     .catch(err => console.error(err))
 })
 
+// 搜尋餐廳
 app.get('/search', (req, res) => {
   if (!req.query.keyword) {
     return res.redirect('/')
@@ -68,6 +86,22 @@ app.get('/search', (req, res) => {
     .catch(err => console.error(err))
 })
 
+// 編輯餐廳頁面
+app.get('/restaurants/:restaurant_id/edit', (req, res) => {
+  const restaurant_id = req.params.restaurant_id
+  Restaurant.findById(restaurant_id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(err => console.error(err))
+})
+
+// 更新餐廳
+app.post('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant_id = req.params.restaurant_id
+  Restaurant.findByIdAndUpdate(restaurant_id, req.body) //放入整個更新後的物件
+    .then(() => res.redirect(`/restaurants/${restaurant_id}`))
+    .catch(err => console.error(err))
+})
 
 // start and listen on the Express server
 app.listen(port, () => {

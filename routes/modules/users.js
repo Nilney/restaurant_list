7bba647255bc4307529e3a0a28ddd4ffa69fd7f2
Yen Lin <lynn575938@outlook.com>
2023-07-12 18:38:09 +0800

    feat: add bcrypt and user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -7,6 +10,23 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exists.')
+      return res.render('register', { name, email, password, confirmPassword })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({ name, email, password: hash }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.error(err))
+  })
+    .catch(err => console.error(err))
 })
 
 module.exports = router

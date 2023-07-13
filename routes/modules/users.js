46400f3ace7e5761b1023cfcd8b2deafa7203f59
Yen Lin <lynn575938@outlook.com>
2023-07-13 14:58:43 +0800

    feat: add user logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -35,4 +35,9 @@ router.post('/register', (req, res) => {
     .catch(err => console.error(err))
 })
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
+
 module.exports = router
